ae2e02afbd2f5f1ca597f811728f8a7470a87833
Hung-Han Chen <chenhungh@gmail.com>
2016-03-04 00:33:59 +0200

    update webpack bundling config to reduce the size of build.js

---
+++ webpack.config.prod.js
@@ -0,0 +1,37 @@
+var webpack = require("webpack");
+
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel'
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devtool: 'cheap-module-source-map', // for faster builds use 'eval'
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      },
+      sourceMap: false,
+      mangle: true
+    })
+  ]
+};
