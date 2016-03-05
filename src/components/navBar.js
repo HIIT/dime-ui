4f71dbdb52fbd96a9d1c58f1b2e3b14932941569
Hung-Han Chen <chenhungh@gmail.com>
2016-03-05 22:31:56 +0200

    add nap bar component

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,23 +1,27 @@
 
 import React, { Component } from 'react'
+import { Link } from 'react-router'
 
 export default class NavBar extends Component {
     render() {
         return (
-            <nav className="navbar navbar-fixed-top navbar-dark bg-inverse">
-                <a className="navbar-brand" href="#">DiMe dashboard</a>
-                <ul className="nav navbar-nav">
-                    <li className="nav-item active">
-                        <a className="nav-link" href="#">Events</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Documents</a>
-                    </li>
-                    <li className="nav-item pull-xs-right">
-                        <a className="nav-link" href="#">Log In</a>
-                    </li>
-                </ul>
-            </nav>
+            <header className="navbar navbar-fixed-top navbar-dark bg-inverse" role="banner">
+                <nav className="nav navbar-nav">
+                    <a className="navbar-brand" href="#">DiMe dashboard</a>
+                    <ul className="nav navbar-nav">
+                        <li className="nav-item">
+                            <Link to="/events" className="nav-link" activeClassName="active">Events</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/documents" className="nav-link" activeClassName="active">Documents</Link>
+                        </li>
+                        <li className="nav-item pull-xs-right">
+                            <a className="nav-link" href="#">Log In</a>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+
         )
     }
 }
