5268421f3ccf4b3d9f4d4d54b76a1114952680f8
Hung-Han Chen <chenhungh@gmail.com>
2016-05-02 13:21:27 +0300

    add new fetchDocuments action, revise the fetchEvents action and the tagConfirm/tagConfirm Cancel action to make both work properly

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,10 @@
 import axios from 'axios'
 import moment from 'moment'
 import store from 'store'
+
 export const EVENT_SEARCH = 'EVENT_SEARCH'
 export const FETCH_EVENTS = 'FETCH_EVENTS'
-const dimeServerAddress = 'localhost:3000'
+const dimeServerAddress = 'localhost:8080'
 
 export function eventSearch (keyword) {
     const username = store.get('username')
@@ -25,7 +26,7 @@ export function fetchEvents () {
     const username = store.get('username')
     const password = store.get('password')
     //const url = `http://${dimeServerAddress}/api/data/events?includePlainTextContent=true`
-    const url = `http://${dimeServerAddress}/api/eventsearch?query=hiit`
+    const url = `http://${dimeServerAddress}/api/data/events`
     const request = axios.get(url, {
         auth: {
             username: username,
@@ -34,6 +35,23 @@ export function fetchEvents () {
     })
     return {
         type: FETCH_EVENTS,
+        payload: request
+    }
+}
+
+export const FETCH_DOCUMENTS = 'FETCH_DOCUMENTS'
+export function fetchDocuments () {
+    const username = store.get('username')
+    const password = store.get('password')
+    const url = `http://${dimeServerAddress}/api/data/informationelements`
+    const request = axios.get(url, {
+        auth: {
+            username: username,
+            password: password
+        }
+    })
+    return {
+        type: FETCH_DOCUMENTS,
         payload: request
     }
 }
@@ -69,16 +87,19 @@ export function logOut() {
 export const TAG_CONFIRM = 'TAG_CONFIRM'
 export const TAG_CONFIRM_CANCEL = 'TAG_CONFIRM_CANCEL'
 
-export function tagConfirm(tag, event) {
-    tag.time = moment().toISOString()
+export function tagConfirm(tag, entity) {
     tag.auto = false
+    tag.actor = 'dime-ui'
+    tag.time = moment().toISOString()
+    let informationElement = entity.targettedResource? entity.targettedResource: entity
+
     const username = store.get('username')
     const password = store.get('password')
-    const url = `http://${dimeServerAddress}/api/data/informationelement/${event.targettedResource.id}/addtags`
+    const url = `http://${dimeServerAddress}/api/data/informationelement/${informationElement.id}/addtag`
     const config =  {
         url: url,
         method: 'post',
-        data: [tag],
+        data: tag,
         auth: {
             username: username,
             password: password
@@ -91,12 +112,48 @@ export function tagConfirm(tag, event) {
     }
 }
 
-export function tagConfirmCancel(tag, event) {
+export function tagConfirmCancel(tag, entity) {
+    tag.auto = true
+    tag.actor = 'dime-ui'
+    tag.time = moment().toISOString()
+    let informationElement = entity.targettedResource? entity.targettedResource: entity
+
     const username = store.get('username')
     const password = store.get('password')
-    const url = `http://${dimeServerAddress}/api/data/informationelement/${event.targettedResource.id}/removetag`
-    const config =  {
-        url: url,
+
+    //remove && add again
+    //const removalUrl = `http://${dimeServerAddress}/api/data/informationelement/${event.targettedResource.id}/removetag`
+    //const removalConfig =  {
+    //    url: removalUrl,
+    //    method: 'post',
+    //    data: tag,
+    //    auth: {
+    //        username: username,
+    //        password: password
+    //    }
+    //}
+    //let request = new Promise((resolve, reject) => {
+    //    axios(removalConfig).then((removalResponse)=> {
+    //        const addUrl = `http://${dimeServerAddress}/api/data/informationelement/${event.targettedResource.id}/addtag`
+    //        const addConfig =  {
+    //            url: addUrl,
+    //            method: 'post',
+    //            data: tag,
+    //            auth: {
+    //                username: username,
+    //                password: password
+    //            }
+    //        }
+    //        axios(addConfig).then((addResponse)=> {
+    //            resolve(addResponse)
+    //        })
+    //    })
+    //})
+
+    //simply add, which will overwritten the exising one
+    const addUrl = `http://${dimeServerAddress}/api/data/informationelement/${informationElement.id}/addtag`
+    const addConfig =  {
+        url: addUrl,
         method: 'post',
         data: tag,
         auth: {
@@ -104,7 +161,8 @@ export function tagConfirmCancel(tag, event) {
             password: password
         }
     }
-    const request = axios(config)
+    let request = axios(addConfig)
+
     return {
         type: TAG_CONFIRM_CANCEL,
         payload: request
@@ -118,7 +176,7 @@ export function setModalOpen(index, posY) {
     return {
         type: MODAL_OPEN,
         payload: {
-            eventIndex: index,
+            entityIndex: index,
             mousePosY: posY
         }
     }
@@ -127,8 +185,46 @@ export function setModalClose(index) {
     return {
         type: MODAL_CLOSE,
         payload: {
-            eventIndex: index,
+            entityIndex: index,
             mousePosY: null
         }
+    }
+}
+
+export const FETCH_TAGS = 'FETCH_TAGS'
+
+export function fetchTags () {
+    const username = store.get('username')
+    const password = store.get('password')
+    const url = `http://${dimeServerAddress}/api/data/events?includePlainTextContent=true`
+    //const url = `http://${dimeServerAddress}/api/eventsearch?query=hiit`
+    const request = axios.get(url, {
+        auth: {
+            username: username,
+            password: password
+        }
+    })
+    return {
+        type: FETCH_TAGS,
+        payload: request
+    }
+}
+
+export const GET_FREQUENT_TAGS = 'GET_FREQUENT_TAGS'
+
+export function getFrequentTags () {
+    const username = store.get('username')
+    const password = store.get('password')
+    const url = `http://${dimeServerAddress}/api/data/events?includePlainTextContent=true`
+    //const url = `http://${dimeServerAddress}/api/eventsearch?query=hiit`
+    const request = axios.get(url, {
+        auth: {
+            username: username,
+            password: password
+        }
+    })
+    return {
+        type: GET_FREQUENT_TAGS,
+        payload: request
     }
 }
