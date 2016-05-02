28a98213c8d5db68627ab3656efdf0ded36c4f1b
Hung-Han Chen <chenhungh@gmail.com>
2016-05-02 13:37:25 +0300

    routes changed because of refactoring documentsPage and eventsPage

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,9 @@ import React from 'react'
 import { Route, IndexRedirect } from 'react-router'
 
 import App from './components/app'
-import EventsPage from './components/eventsPage'
-import DocumentsPage from './components/documentsPage'
+import EventsPage from './containers/eventsPage'
+import DocumentsPage from './containers/documentsPage'
+import DashBoard from './components/dashboard'
 import LogInForm from './containers/logInForm'
 
 //<IndexRoute component={EventsPage} />
@@ -12,6 +13,7 @@ export default (
         <IndexRedirect to="/events" />
         <Route path="/events" component={EventsPage}/>
         <Route path="/documents" component={DocumentsPage}/>
+        <Route path="/dashboard" component={DashBoard}/>
         <Route path="/login" component={LogInForm}/>
     </Route>
 )
