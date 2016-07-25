fa28d2c617ad2e5814a0190ae827e1a7f79add54
Hung-Han Chen <chenhungh@gmail.com>
2016-07-25 18:18:53 +0300

    add delete button

--- src/components/entityCard.js
+++ src/components/entityCard.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { Component } from 'react'
 import moment from 'moment'
 import Transition from 'react-motion-ui-pack'
-
+import MdDelete from 'react-icons/lib/md/delete'
 import { setModalOpen } from '../actions/index'
 import { purify } from '../services/purifyText'
 
 export const entityCard = {
-    cursor: 'pointer',
     wordWrap: 'break-word',
+}
+
+const entityCardBody = {
+    cursor: 'pointer',
 }
 export const entityCardPlainText = {
     fontSize: '9px',
@@ -29,6 +32,15 @@ export const entityCardHeaderUrl = {
     fontSize: '9px',
 }
 
+const entityCardDelete = {
+    cursor: 'pointer',
+    display: 'inline-block',
+    marginLeft: '5px',
+    marginTop: '-5px',
+    fontSize: '16px',
+    color: 'rgba(0,0,0,.2)'
+}
+
 class EntityCard extends Component {
     renderPlainTextContent(informationElement) {
         return (
@@ -42,7 +54,7 @@ class EntityCard extends Component {
             </p>
         )
     }
-    entityCardRender(entity, index) {
+    entityCardRender(entity, entityIndex) {
         let informationElement = entity.targettedResource? entity.targettedResource: entity
         //console.log(entity)
         return (
@@ -61,18 +73,27 @@ class EntityCard extends Component {
             //        opacity: 0
             //     }}
             //>
-            <div className="card" style={entityCard} key={index}>
+            <div className="card" style={entityCard} key={entityIndex}>
                 <div className="card-header clearfix" style={entityCardHeader}>
                     {entity.actor?
                         <span className="pull-xs-left" style={entityCardHeaderSpan}>
                             From: <b style={entityCardHeaderTitle}>{entity.actor}</b>
                         </span>
                     : null}
+                    <span className="pull-xs-right"
+                          style={entityCardDelete}
+                          onClick={(mouseEvent) => this.handleClickOnDelete(mouseEvent, entity)}
+                    >
+                        <MdDelete />
+                    </span>
                     <span className="pull-xs-right" style={entityCardHeaderUrl}>
                         {`${moment(entity.timeCreated).format('MMMM Do YYYY, HH:mm:ss')}`}
                     </span>
                 </div>
-                <div className="card-block clearfix">
+                <div className="card-block clearfix"
+                     onClick={(mouseEvent) => this.handleClickOnEntity(entityIndex, mouseEvent)}
+                     style={entityCardBody}
+                >
                     <a className="" href={`${informationElement.uri}`} style={{color:'rgba(0,0,0,.9)'}}>
                         {informationElement.title}
                     </a>
@@ -87,25 +108,25 @@ class EntityCard extends Component {
                         </span>
                         </a>
                     </h5>
-
-
                 </div>
             </div>
             //</Transition>
         )
     }
-    handleClickOnEvent(entityIndex, mouseEvent) {
+    handleClickOnEntity(entityIndex, mouseEvent) {
         if (this.props.setModalOpen) {
             let posY = mouseEvent.nativeEvent.screenY
             this.props.setModalOpen(entityIndex, posY)
         }
+    }
+    handleClickOnDelete(mouseEvent, entity) {
+        console.log(entity)
     }
     render() {
         let {entityIndex, entity} = this.props
         return (
             <div
                 className={`${this.props.className}`}
-                onClick={(mouseEvent) => this.handleClickOnEvent(entityIndex, mouseEvent)}
             >
                 {this.entityCardRender(entity, entityIndex)}
             </div>
