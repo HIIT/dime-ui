16d6d45ac52a2a48e603a38248143049918d928d
Hung-Han Chen <chenhungh@gmail.com>
2016-07-25 18:18:11 +0300

    init the dashboard page

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -3,12 +3,20 @@ import { Component } from 'react'
 
 //import NavLink from '../components/NavLink'
 import FrequentTagsList from '../containers/frequentTagsList'
+import Profile from '../containers/profile'
 
 export default class DashBoard extends Component {
     render() {
         return (
-            <div>
-                <FrequentTagsList />
+            <div
+                className="container-fluid"
+            >
+                <div className="col-xs-10">
+                    <Profile />
+                </div>
+                <div className="col-xs-2">
+                    <FrequentTagsList />
+                </div>
             </div>
         );
     }
