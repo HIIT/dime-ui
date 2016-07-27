d490f9c99fa1aefd223d55cc5b8e820190740795
Hung-Han Chen <chenhungh@gmail.com>
2016-07-27 12:30:39 +0300

    add bable-polyfill, move store and root element into separate files

--- src/index.js
+++ src/index.js
@@ -1,25 +1,12 @@
+import  'babel-polyfill'
 import React from 'react'
-import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'redux'
-import ReduxPromise from 'redux-promise'
-import { Router, browserHistory} from 'react-router'
-import { syncHistoryWithStore, routerMiddleware} from 'react-router-redux'
+import { render } from 'react-dom'
+import configureStore from './configureStore'
+import Root from './components/Root'
 
-import reducers from './reducers'
-import routes from './routes'
+const store = configureStore()
 
-const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
-const middleware = routerMiddleware(browserHistory)
-const store = createStoreWithMiddleware(
-    reducers,
-    applyMiddleware(middleware)
-)
-const history = syncHistoryWithStore(browserHistory, store)
-//history.listen(location => console.log(location))
-
-ReactDOM.render(
-  <Provider store={store}>
-    <Router history={history} routes={routes}/>
-  </Provider>
-  , document.querySelector('.reactRoot'));
+render(
+  <Root store={store} />
+  , document.querySelector('.reactRoot')
+);
