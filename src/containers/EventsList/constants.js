5d25a5be38462d48ba998d7da640326ac67fef38
Hung-Han Chen <chenhungh@gmail.com>
2016-08-15 21:56:07 +0300

    Remove default example

--- src/containers/EventsList/constants.js
+++
@@ -1,3 +0,0 @@
-export const LOAD_EVENTS = 'eventsList/LOAD_EVENTS'
-export const LOAD_EVENTS_SUCCESS = 'eventsList/LOAD_EVENTS_SUCCESS'
-export const LOAD_EVENTS_ERROR = 'eventsList/LOAD_EVENTS_ERROR'
