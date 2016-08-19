7768f6b5f0d4a9c515d0fc8a49a076e92ee971df
Hung-Han Chen <chenhungh@gmail.com>
2016-08-19 13:48:01 +0300

    add saga to eventslist

--- app/containers/EventsList/sagas.js
+++ app/containers/EventsList/sagas.js
@@ -1,11 +1,38 @@
-// import { take, call, put, select } from 'redux-saga/effects';
+import { take, call, put, select, fork, cancel, takeLatest } from 'redux-saga/effects';
+import { LOCATION_CHANGE } from 'react-router-redux';
+import { LOAD_EVENTS } from './constants';
+import request from 'utils/request';
+import { selectAuth } from './selectors';
+import { eventsLoaded, eventsLoadingError } from './actions';
 
-// Individual exports for testing
-export function* defaultSaga() {
-  return;
+export function* getEvents() {
+  const { username, password } = yield select(selectAuth());
+  const base64Toekn = window.btoa(`${username}:${password}`);
+  const requestURL = `http://${window.location.hostname}:${window.location.port}$/api/data/events`;
+  const options = {
+    method: 'GET',
+    headers: {
+      Authorization: `Basic ${base64Toekn}`,
+    },
+  };
+  const respond = yield call(request, requestURL, options);
+  if (!respond.err) {
+    yield put(eventsLoaded(respond.data));
+  } else {
+    yield put(eventsLoadingError(respond.err));
+  }
 }
 
-// All sagas to be loaded
+export function* getEventsWatcher() {
+  yield* takeLatest(LOAD_EVENTS, getEvents);
+}
+
+export function* eventsData() {
+  const watcher = yield fork(getEventsWatcher);
+  yield take(LOCATION_CHANGE);
+  yield cancel(watcher);
+}
+
 export default [
-  defaultSaga,
+  eventsData,
 ];
