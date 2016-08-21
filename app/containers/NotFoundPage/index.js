e15cd24be9d94d4ac04ea5b92418ebffdd1c064e
Hung-Han Chen <chenhungh@gmail.com>
2016-08-21 22:39:30 +0300

    polishing NotFoundPage

--- app/containers/NotFoundPage/index.js
+++ app/containers/NotFoundPage/index.js
@@ -12,14 +12,19 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import messages from './messages';
+import styles from './styles.css'
 
 export default class NotFound extends React.Component { // eslint-disable-line react/prefer-stateless-function
 
   render() {
     return (
-      <h1>
-        <FormattedMessage {...messages.header} />
-      </h1>
+      <div
+        className={styles.notFoundWrapper}
+      >
+        <h1>
+          <FormattedMessage {...messages.header} />
+        </h1>
+      </div>
     );
   }
 }
