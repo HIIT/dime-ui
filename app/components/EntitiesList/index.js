56744424165a3546d1fc019521e01dbcf02d8832
Hung-Han Chen <chenhungh@gmail.com>
2016-08-21 22:33:36 +0300

    add searchBox in entitiesList

--- app/components/EntitiesList/index.js
+++ app/components/EntitiesList/index.js
@@ -7,6 +7,7 @@
 import React from 'react';
 import ReactList from 'react-list';
 import { Grid, Row, Col } from 'react-flexbox-grid/lib/index';
+import TextField from 'material-ui/TextField';
 import TagsList from 'components/TagsList';
 import EntityCard from 'components/EntityCard';
 import styles from './styles.css';
@@ -29,8 +30,9 @@ export class EntitiesList extends React.Component { // eslint-disable-line react
     return (
       <Row
         key={entity.id}
+        className={styles.entityWrapper}
       >
-        <Col xsOffset={1} xs={1} smOffset={1} sm={2} >
+        <Col xsOffset={0} xs={1} smOffset={2} sm={2} >
           <TagsList
             entityID={entity.id}
             tags={autoTags}
@@ -44,7 +46,7 @@ export class EntitiesList extends React.Component { // eslint-disable-line react
             className={`label-sucess ${styles.notAutoTag}`}
           />
         </Col>
-        <Col xs={8} sm={6} >
+        <Col xsOffset={1} xs={8} smOffset={0} sm={6} >
           <EntityCard
             entity={entity}
             clickOnEntityCard={this.props.clickOnEntityCard}
@@ -54,14 +56,32 @@ export class EntitiesList extends React.Component { // eslint-disable-line react
       </Row>
     );
   }
+  renderSearchBox = () => {
+    const hint = 'Search';
+    const floatedHint = 'Keyword';
+    return (
+      <Row>
+        <Col xsOffset={2} xs={8} smOffset={4} sm={6} >
+          <TextField
+            hintText={hint}
+            floatingLabelText={floatedHint}
+            fullWidth
+          />
+        </Col>
+      </Row>
+    );
+  }
   render() {
     return (
-      <Grid>
+      <Grid
+        className={styles.entitiesListWrapper}
+      >
+        {this.renderSearchBox()}
         <ReactList
           itemRenderer={this.renderEntity}
           length={this.props.entities.length}
-          pageSize={6}
-          threshold={400}
+          pageSize={12}
+          threshold={450}
           useTranslate3d // TODO:check browsers support for Translate3d
         />
       </Grid>
