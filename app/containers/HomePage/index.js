6fe4577d0f51119dc76a55f5860b41d9381d6caf
Hung-Han Chen <chenhungh@gmail.com>
2016-08-29 23:58:27 +0300

    redirect to events page by default

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -1,25 +1,31 @@
 /*
  * HomePage
  *
- * This is the first thing users see of our App, at the '/' route
- *
- * NOTE: while this component should technically be a stateless functional
- * component (SFC), hot reloading does not currently support SFCs. If hot
- * reloading is not a neccessity for you then you can refactor it and remove
- * the linting exception.
  */
 
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
-import messages from './messages';
+import { connect } from 'react-redux';
+import { push } from 'react-router-redux';
 
-export default class HomePage extends React.Component { // eslint-disable-line react/prefer-stateless-function
-
+class HomePage extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  static propTypes = {
+    changeRoute: React.PropTypes.func,
+  };
+  componentWillMount() {
+    this.props.changeRoute('/events');
+  }
   render() {
     return (
-      <h1>
-        <FormattedMessage {...messages.header} />
-      </h1>
+      <div></div>
     );
   }
 }
+
+function mapDispatchToProps(dispatch) {
+  return {
+    changeRoute: (url) => dispatch(push(url)),
+    dispatch,
+  };
+}
+
+export default connect(null, mapDispatchToProps)(HomePage);
