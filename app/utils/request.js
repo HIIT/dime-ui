0e3f3ba361cf91a048751142225d1d53032b3620
Hung-Han Chen <chenhungh@gmail.com>
2016-08-29 11:13:58 +0300

    add handler for 204 and its empty respond body

--- app/utils/request.js
+++ app/utils/request.js
@@ -1,28 +1,16 @@
 import 'whatwg-fetch';
 
 /**
- * Parses the JSON returned by a network request
- *
- * @param  {object} response A response from a network request
- *
- * @return {object}          The parsed JSON from the request
- */
-function parseJSON(response) {
-  return response.json();
-}
-
-/**
  * Checks if a network request came back fine, and throws an error if not
  *
  * @param  {object} response   A response from a network request
  *
  * @return {object|undefined} Returns either the response, or throws an error
  */
-function checkStatus(response) {
-  if (response.status >= 200 && response.status < 300) {
-    return response;
+function checkResponse(response) {
+  if (response.ok) {
+    return response[response.status === 204 ? 'text' : 'json']();
   }
-
   const error = new Error(response.statusText);
   error.response = response;
   throw error;
@@ -38,8 +26,7 @@ function checkStatus(response) {
  */
 export default function request(url, options) {
   return fetch(url, options)
-    .then(checkStatus)
-    .then(parseJSON)
+    .then(checkResponse)
     .then((data) => ({ data }))
     .catch((err) => ({ err }));
 }
