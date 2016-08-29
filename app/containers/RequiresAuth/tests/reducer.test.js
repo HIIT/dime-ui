df5b9f9df1e5f2a04449206fdde7a645dec051a6
Hung-Han Chen <chenhungh@gmail.com>
2016-08-29 11:13:24 +0300

    remove unnecessary tests

--- app/containers/RequiresAuth/tests/reducer.test.js
+++
@@ -1,9 +0,0 @@
-import expect from 'expect';
-import requiresAuthReducer from '../reducer';
-import { fromJS } from 'immutable';
-
-describe('requiresAuthReducer', () => {
-  it('returns the initial state', () => {
-    expect(requiresAuthReducer(undefined, {})).toEqual(fromJS({}));
-  });
-});
