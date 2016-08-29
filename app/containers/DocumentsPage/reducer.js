8805944e81bfe81c89481234604682ae613d6566
Hung-Han Chen <chenhungh@gmail.com>
2016-08-29 23:57:18 +0300

    new state change on delete/tagconfirm

--- app/containers/DocumentsPage/reducer.js
+++ app/containers/DocumentsPage/reducer.js
@@ -9,11 +9,17 @@
    LOAD_DOCUMENTS,
    LOAD_DOCUMENTS_SUCCESS,
    LOAD_DOCUMENTS_ERROR,
+   DELETE_DOCUMENT,
+   DELETE_DOCUMENT_SUCESS,
+   DELETE_DOCUMENT_ERROR,
+   CLICK_DOCUMENT_TAG,
+   TOOGLE_DOCUMENT_TAG_SUCESS,
+   TOOGLE_DOCUMENT_TAG_ERROR,
  } from './constants';
 
  const initialState = fromJS({
    loading: false,
-   error: false,
+   error: {},
    data: [],
  });
 
@@ -22,17 +28,44 @@
      case LOAD_DOCUMENTS:
        return state
          .set('loading', true)
-         .set('error', false)
          .set('data', fromJS([]));
      case LOAD_DOCUMENTS_SUCCESS:
        return state
          .set('data', fromJS(action.documents))
-         .set('loading', false);
+         .set('loading', false)
+         .set('error', fromJS({}));
      case LOAD_DOCUMENTS_ERROR:
        return state
-         .set('error', action.error)
+         .set('data', fromJS([]))
          .set('loading', false)
-         .set('data', fromJS([]));
+         .set('error', fromJS(action.error));
+     case DELETE_DOCUMENT:
+       return state
+         .set('loading', true);
+     case DELETE_DOCUMENT_SUCESS:
+       return state
+         .set('loading', false)
+         .set('error', fromJS({}))
+         .deleteIn(['data', state.get('data').findIndex((item) => item.get('id') === action.documentID)]);
+     case DELETE_DOCUMENT_ERROR:
+       return state
+         .set('loading', false)
+         .set('error', fromJS(action.error));
+     case CLICK_DOCUMENT_TAG:
+       return state
+         .set('loading', true);
+     case TOOGLE_DOCUMENT_TAG_SUCESS: {
+       const informationDocumentIndex = state.get('data').findIndex((item) => item.get('id') === action.respond.id);
+       const newInfoDocWithNewTags = fromJS(action.respond);
+       return state
+         .set('loading', false)
+         .set('error', fromJS({}))
+         .setIn(['data', informationDocumentIndex, 'tags'], newInfoDocWithNewTags.getIn(['tags']));
+     }
+     case TOOGLE_DOCUMENT_TAG_ERROR:
+       return state
+         .set('loading', false)
+         .set('error', fromJS(action.error));
      default:
        return state;
    }
