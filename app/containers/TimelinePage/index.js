0a91b338434f55f390b8e13493843ca17ea429fb
Hung-Han Chen <chenhungh@gmail.com>
2016-09-01 13:16:52 +0300

    before deleting some elements

--- app/containers/TimelinePage/index.js
+++ app/containers/TimelinePage/index.js
@@ -8,6 +8,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { createStructuredSelector } from 'reselect';
+import flatten from 'lodash/flatten';
 import { loadDocuments, loadEvents } from './actions';
 import { selectDocuments, selectEvents, selectLoading, selectError } from './selectors';
 import styles from './styles.css';
@@ -17,6 +18,18 @@ const timeStamps = ['0:00', '1:00', '2:00', '3:00', '4:00', '5:00', '6:00', '7:0
 function getTimeInMins(time) {
   return (time.getHours() * 60) + time.getMinutes();
 }
+
+function getFrequentWords(keywords, maxNumber) {
+  const frequencies = {};
+  for (const word of keywords) {
+    frequencies[word] = frequencies[word] || 0;
+    frequencies[word]++;
+  }
+  const words = Object.keys(frequencies);
+  const wordsRankedByFrequent = words.sort((a, b) => frequencies[b] - frequencies[a]).slice(0, maxNumber);
+  return wordsRankedByFrequent;
+}
+
 export class TimelinePage extends React.Component { // eslint-disable-line react/prefer-stateless-function
   static propTypes = {
     events: React.PropTypes.array,
@@ -28,29 +41,141 @@ export class TimelinePage extends React.Component { // eslint-disable-line react
   }
   componentWillMount() {
     this.props.loadEvents();
-    this.props.loadDocuments();
+  }
+  onMouseEnterEventCircleHandler() {
+
+  }
+  onMouseLeaveEventCircleHandler() {
+
+  }
+  renderTags(event) {
+    if (event.targettedResource && event.targettedResource.tags) {
+      const tags = event.targettedResource.tags;
+      return (
+        <div>
+          {tags.map((tag) => <span className={styles.pointText} key={tag.id}> { tag.text } </span>)}
+        </div>
+      );
+    }
+    return null;
   }
   render() {
     const { events } = this.props;
+    const walkStartStopEvents = events.filter((event) => {
+      if (event.activityType === 'walkstartstop') {
+        return true;
+      }
+      return false;
+    });
     return (
       <div className={styles.timelineContainer}>
         <div className={styles.timeline}>
           <div className={styles.timePointsWrapper}>
-            {events.map((event, index) => {
+            {events.map((event) => {
               const time = new Date(event.start);
               const timeInMinute = getTimeInMins(time);
               const timeInPercent = (timeInMinute / 1440) * 100;
-              if (index > 0 && index < (events.length - 1)) {
-                const room = Math.abs(timeInMinute - getTimeInMins(new Date(events[index - 1].start))) + Math.abs(timeInMinute - getTimeInMins(new Date(events[index + 1].start)));
-                const max = 10;
-                const marginTopDistance = max - room;
+              if (event.activityType === 'steps') {
                 return (
-                  <div className={styles.point} key={event.id} style={{ left: `${timeInPercent}%` }}></div>
+                  <div
+                    className={styles.period}
+                    key={event.id}
+                    style={{
+                      left: `${timeInPercent}%`,
+                      width: `${(event.duration / (1440 * 60)) * 100}%`,
+                      height: `${event.value}px`,
+                      top: `-${event.value - 8}px`,
+                    }}
+                  >
+                  </div>
+                );
+              }
+              if (event.activityType === 'heart') {
+                return (
+                  <div
+                    className={styles.period}
+                    key={event.id}
+                    style={{
+                      left: `${timeInPercent}%`,
+                      width: '1px',
+                      height: '5px',
+                      background: `hsl(${100 - (event.value / 1.4)}, 100%, 50%)`,
+                    }}
+                  >
+                  </div>
                 );
               }
               return (
-                <div className={styles.point} key={event.id} style={{ left: `${timeInPercent}%` }}></div>
+                <div
+                  className={styles.point}
+                  key={event.id}
+                  style={{ left: `${timeInPercent}%` }}
+                  onMouseEnter={this.onMouseEnterEventCircleHandler}
+                  onMouseLeave={this.onMouseLeaveEventCircleHandler}
+                >
+                  {this.renderTags(event)}
+                </div>
               );
+            })}
+            {walkStartStopEvents.map((walkStartStopEvent, index) => {
+              if (walkStartStopEvent.value === 1) {
+                const startTime = getTimeInMins(new Date(walkStartStopEvent.start)) / 14.4;
+                const endTime = getTimeInMins(new Date(walkStartStopEvents[index + 1].start)) / 14.4;
+                const walkingDuration = endTime - startTime;
+                if (walkStartStopEvent.activityType === 'walkstartstop') {
+                  return (
+                    <div
+                      className={styles.period}
+                      key={walkStartStopEvent.id}
+                      style={{
+                        background: 'rgba(0, 255, 205, 0.1)',
+                        marginTop: '-20%',
+                        left: `${startTime}%`,
+                        width: `${walkingDuration}%`,
+                        height: '300%',
+                      }}
+                    >
+                    </div>
+                  );
+                }
+              } else if (walkStartStopEvent.value === 0 && walkStartStopEvent && walkStartStopEvents[index + 1]) {
+                const startTime = getTimeInMins(new Date(walkStartStopEvent.start)) / 14.4;
+                const endTime = getTimeInMins(new Date(walkStartStopEvents[index + 1].start)) / 14.4;
+                const walkingDuration = endTime - startTime;
+                const workingEventsTags = flatten(this.props.events.filter((event) => {
+                  const eventTime = getTimeInMins(new Date(event.start)) / 14.4;
+                  if (startTime < eventTime && eventTime < endTime && event.activityType !== 'heart' && event.activityType !== 'steps') {
+                    return true;
+                  }
+                  return false;
+                }).map((event) => event.targettedResource.tags.map((tag) => tag.text)));
+                const mostUsed = getFrequentWords(workingEventsTags, 8);
+                if (walkStartStopEvent.activityType === 'walkstartstop') {
+                  return (
+                    <div
+                      className={styles.period}
+                      key={walkStartStopEvent.id}
+                      style={{
+                        background: 'rgba(0, 255, 205, 1)',
+                        marginTop: '-23%',
+                        left: `${startTime}%`,
+                        width: `${walkingDuration}%`,
+                        height: '400%',
+                      }}
+                    >
+                      <div
+                        className={styles.workingKeywordWrapper}
+                        style={{
+                          width: `${walkingDuration}%`,
+                        }}
+                      >
+                        {mostUsed.map((word, wIndex) => <span className={styles.workingKeyword} key={`word ${wIndex}`}>{word} </span>)}
+                      </div>
+                    </div>
+                  );
+                }
+              }
+              return null;
             })}
             {timeStamps.map((time, index) => <div className={styles.timeStamp} key={`time ${index}`} style={{ left: `${(100 / 24) * index}%` }}> {time}</div>)}
           </div>
