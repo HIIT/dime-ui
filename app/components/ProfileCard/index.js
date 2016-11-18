0e2b5f1697f3c4843c6efba30b71d2d3c6a5b45a
Hung-Han Chen <chenhungh@gmail.com>
2016-11-18 16:10:26 +0200

    refactor profile card

--- app/components/ProfileCard/index.js
+++ app/components/ProfileCard/index.js
@@ -5,13 +5,14 @@
 */
 
 import React from 'react';
-import { Card, CardActions, CardHeader, CardText } from 'material-ui/Card';
+import { Card, CardText } from 'material-ui/Card';
 import ModeEdit from 'material-ui/svg-icons/editor/mode-edit';
 import ActionDelete from 'material-ui/svg-icons/action/delete';
 import NavigationCancel from 'material-ui/svg-icons/navigation/cancel';
 import ContentSave from 'material-ui/svg-icons/content/save';
 import RaisedButton from 'material-ui/RaisedButton';
 import { Row, Col } from 'react-flexbox-grid/lib/index';
+import TextField from 'material-ui/TextField';
 import { WithContext as TagsInput } from 'react-tag-input';
 import TagsList from 'components/TagsList';
 import ProfileEntitiesList from 'components/ProfileEntitiesList';
@@ -20,25 +21,27 @@ import styles from './styles.css';
 
 export class ProfileCard extends React.Component { // eslint-disable-line react/prefer-stateless-function
   static propTypes = {
-    clickOnProfileCard: React.PropTypes.func,
     editProfile: React.PropTypes.func,
     cancelEditProfile: React.PropTypes.func,
+    saveProfile: React.PropTypes.func,
     deleteProfile: React.PropTypes.func,
     profile: React.PropTypes.object,
     clickOnProfileTag: React.PropTypes.func,
-    clickOnEntitiyTag: React.PropTypes.func,
+    clickOnEntityTag: React.PropTypes.func,
+    clickOnEntityDelete: React.PropTypes.func,
+    clickOnEntity: React.PropTypes.func,
   }
-  handleClickOnDelete = (profile, mouseEvent) => {
-    mouseEvent.preventDefault();
-    this.props.deleteProfile(profile.id);
-  }
-  handleClickOnEdit = (profile, mouseEvent) => {
-    mouseEvent.preventDefault();
+  handleClickOnEdit = (profile) => {
     this.props.editProfile(profile.id);
   }
-  handleClickOnCancelEdit = (profile, mouseEvent) => {
-    mouseEvent.preventDefault();
+  handleClickOnCancelEdit = (profile) => {
     this.props.cancelEditProfile(profile.id);
+  }
+  handleClickOnSave = (profile) => {
+    this.props.saveProfile(profile);
+  }
+  handleClickOnDelete = (profile) => {
+    this.props.deleteProfile(profile.id);
   }
   handleTagAddition = (tag) => {
     this.props.profile.tags.push({ id: this.props.profile.tags.length + 1, text: tag });
@@ -49,12 +52,19 @@ export class ProfileCard extends React.Component { // eslint-disable-line react/
     this.props.profile.tags = tags;
   }
   renderProfileCardHeader = (profile) => {
-    const title = profile.name;
-    const { editing } = profile;
+    const { editing, name } = profile;
     return (
-      <CardHeader
-        title={title}
-      >
+      <div className={styles.cardHeader}>
+        <div
+          className={styles.cardHeaderTitleWrapper}
+        >
+          <TextField
+            defaultValue={name}
+            floatingLabelText="Profile Name"
+            disabled={!editing}
+            style={{ width: '140px' }}
+          />
+        </div>
         <div
           className={styles.cardHeaderDeleteWrapper}
         >
@@ -100,99 +110,99 @@ export class ProfileCard extends React.Component { // eslint-disable-line react/
             />
           </div>
         : null }
-      </CardHeader>
+      </div>
     );
   }
+  renderValidatedEntitiesList = (profile, editing) =>
+    <div className={styles.validatedEntitiesWrapper}>
+      { profile.validatedInformationElements.length > 0 ?
+        <ProfileEntitiesList
+          listTitle="Validated Documents"
+          entities={profile.validatedInformationElements}
+          editing={editing}
+        />
+      : null}
+      { profile.validatedEvents.length > 0 ?
+        <ProfileEntitiesList
+          listTitle="Validated Events"
+          entities={profile.validatedEvents}
+          editing={editing}
+        />
+      : null}
+    </div>
+  renderSuggestedEntitiesList = (profile, editing) =>
+    <div className={styles.suggestedEntitiesWrapper}>
+      { profile.suggestedInformationElements.length > 0 ?
+        <ProfileEntitiesList
+          listTitle="Suggested Documents"
+          entities={profile.suggestedInformationElements}
+          editing={editing}
+          clickOnEntityTag={this.props.clickOnEntityTag}
+        />
+      : null}
+      { profile.suggestedEvents.length > 0 ?
+        <ProfileEntitiesList
+          listTitle="Suggested Events"
+          entities={profile.suggestedEvents}
+          editing={editing}
+          clickOnEntityTag={this.props.clickOnEntityTag}
+        />
+      : null}
+    </div>
   renderProfileCardBody = (profile) => {
     const { editing, tags } = profile;
     return (
-      <CardText>
-        <Row>
-          <Col
-            xs={2}
-          >
-            <h4>Tags</h4>
-            { editing ?
-              <TagsInput
-                tags={tags}
-                handleDelete={this.handleTagDelete}
-                handleAddition={this.handleTagAddition}
-                classNames={{
-                  tagInput: `${styles.profileTagsInput}`,
-                  selected: `${styles.profileTagsSelected}`,
-                  tag: `${styles.profileTagsEditing}`,
-                  remove: `${styles.profileTagsRemove}`,
-                  suggestions: `${styles.profileTagSuggestion}`,
-                }}
-              />
-            :
-              <TagsList
-                entityID={profile.id}
-                tags={profile.tags}
-                clickOnTag={this.props.clickOnProfileTag}
-                className={styles.profileTags}
-              />
-            }
-          </Col>
-          <Col
-            xs={2}
-          >
-            <div className={styles.suggestedEntitiesWrapper}>
-              { profile.suggestedInformationElements.length > 0 ?
-                <ProfileEntitiesList
-                  listTitle="Suggested Documents"
-                  entities={profile.suggestedInformationElements}
-                  editing={editing}
-                  clickOnEntitiyTag={this.props.clickOnEntitiyTag}
+      <div className={styles.profileCardBodyWrapper}>
+        <CardText>
+          <Row>
+            <Col
+              xs={2}
+            >
+              <h4>Tags</h4>
+              { editing ?
+                <TagsInput
+                  tags={tags}
+                  handleDelete={this.handleTagDelete}
+                  handleAddition={this.handleTagAddition}
+                  classNames={{
+                    tagInput: `${styles.profileTagsInput}`,
+                    selected: `${styles.profileTagsSelected}`,
+                    tag: `${styles.profileTagsEditing}`,
+                    remove: `${styles.profileTagsRemove}`,
+                    suggestions: `${styles.profileTagSuggestion}`,
+                  }}
                 />
-              : null}
-              { profile.suggestedEvents.length > 0 ?
-                <ProfileEntitiesList
-                  listTitle="Suggested Events"
-                  entities={profile.suggestedEvents}
-                  editing={editing}
-                  clickOnEntitiyTag={this.props.clickOnEntitiyTag}
+              :
+                <TagsList
+                  entityID={profile.id}
+                  tags={profile.tags}
+                  clickOnTag={this.props.clickOnProfileTag}
+                  className={styles.profileTags}
                 />
-              : null}
-            </div>
-          </Col>
-          <Col
-            xs={4}
-          >
-            { profile.validatedInformationElements.length > 0 ?
-              <ProfileEntitiesList
-                listTitle="Validated Documents"
-                entities={profile.validatedInformationElements}
-                editing={editing}
-              />
-            : null}
-            { profile.validatedEvents.length > 0 ?
-              <ProfileEntitiesList
-                listTitle="Validated Events"
-                entities={profile.validatedEvents}
-                editing={editing}
-              />
-            : null}
-          </Col>
-        </Row>
-      </CardText>
+              }
+            </Col>
+            <Col
+              xs={2}
+            >
+              {this.renderSuggestedEntitiesList(profile, editing)}
+            </Col>
+            <Col
+              xs={4}
+            >
+              {this.renderValidatedEntitiesList(profile, editing)}
+            </Col>
+          </Row>
+        </CardText>
+      </div>
     );
   }
-  renderProfileCardActions = () =>
-    <CardActions>
-    </CardActions>
   render() {
     const { profile } = this.props;
     return (
-      <div
-        className={styles.cardWrapper}
-      >
-        <Card>
-          {this.renderProfileCardHeader(profile)}
-          {this.renderProfileCardBody(profile)}
-          {this.renderProfileCardActions(profile)}
-        </Card>
-      </div>
+      <Card>
+        {this.renderProfileCardHeader(profile)}
+        {this.renderProfileCardBody(profile)}
+      </Card>
     );
   }
 }
