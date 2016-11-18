45fba8102746454f8e7ec0fd2ee27b0d1ca3dd0f
Hung-Han Chen <chenhungh@gmail.com>
2016-11-18 16:16:57 +0200

    the indent was 1, it should be 2,4,6...

--- app/containers/ProfilesPage/index.js
+++ app/containers/ProfilesPage/index.js
@@ -4,56 +4,76 @@
  *
  */
 
- import React from 'react';
- import { bindActionCreators } from 'redux';
- import { connect } from 'react-redux';
- import { createStructuredSelector } from 'reselect';
- import { selectProfiles } from './selectors';
- import { loadProfiles, searchProfile, createProfile, editProfile, cancelEditProfile, deleteProfile, clickOnEntityTag } from './actions';
- import requiresAuth from 'containers/RequiresAuth';
- import ProfilesTab from 'components/ProfilesTab';
+import React from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { selectProfiles } from './selectors';
+import {
+ loadProfiles,
+ createProfile,
+ editProfile,
+ cancelEditProfile,
+ saveProfile,
+ deleteProfile,
+ clickOnProfileTag,
+ clickOnEntityTag,
+ clickOnEntityDelete,
+ clickOnEntity,
+} from './actions';
+import requiresAuth from 'containers/RequiresAuth';
+import ProfilesTab from 'components/ProfilesTab';
 
- export class ProfilesPage extends React.Component { // eslint-disable-line react/prefer-stateless-function
-   static propTypes = {
-     profiles: React.PropTypes.array,
-     loadProfiles: React.PropTypes.func,
-     searchProfile: React.PropTypes.func,
-     createProfile: React.PropTypes.func,
-     editProfile: React.PropTypes.func,
-     cancelEditProfile: React.PropTypes.func,
-     deleteProfile: React.PropTypes.func,
-     clickOnEntityTag: React.PropTypes.func,
-   }
-   render() {
-     return (
-       <ProfilesTab
-         profiles={this.props.profiles}
-         initProfilesList={this.props.loadProfiles}
-         search={this.props.searchProfile}
-         createProfile={this.props.createProfile}
-         editProfile={this.props.editProfile}
-         cancelEditProfile={this.props.cancelEditProfile}
-         deleteProfile={this.props.deleteProfile}
-         clickOnEntityTag={this.props.clickOnEntityTag}
-       />
-     );
-   }
- }
+export class ProfilesPage extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  static propTypes = {
+    profiles: React.PropTypes.array,
+    loadProfiles: React.PropTypes.func,
+    createProfile: React.PropTypes.func,
+    editProfile: React.PropTypes.func,
+    cancelEditProfile: React.PropTypes.func,
+    saveProfile: React.PropTypes.func,
+    deleteProfile: React.PropTypes.func,
+    clickOnProfileTag: React.PropTypes.func,
+    clickOnEntityTag: React.PropTypes.func,
+    clickOnEntityDelete: React.PropTypes.func,
+    clickOnEntity: React.PropTypes.func,
+  }
+  render() {
+    return (
+      <ProfilesTab
+        profiles={this.props.profiles}
+        initProfilesList={this.props.loadProfiles}
+        createProfile={this.props.createProfile}
+        editProfile={this.props.editProfile}
+        cancelEditProfile={this.props.cancelEditProfile}
+        saveProfile={this.props.saveProfile}
+        deleteProfile={this.props.deleteProfile}
+        clickOnProfileTag={this.props.clickOnProfileTag}
+        clickOnEntityTag={this.props.clickOnEntityTag}
+        clickOnEntityDelete={this.props.clickOnEntityDelete}
+        clickOnEntity={this.props.clickOnEntity}
+      />
+   );
+  }
+}
 
- const mapStateToProps = createStructuredSelector({
-   profiles: selectProfiles(),
- });
+const mapStateToProps = createStructuredSelector({
+  profiles: selectProfiles(),
+});
 
- function mapDispatchToProps(dispatch) {
-   return {
-     loadProfiles: bindActionCreators(loadProfiles, dispatch),
-     searchProfile: bindActionCreators(searchProfile, dispatch),
-     createProfile: bindActionCreators(createProfile, dispatch),
-     editProfile: bindActionCreators(editProfile, dispatch),
-     cancelEditProfile: bindActionCreators(cancelEditProfile, dispatch),
-     deleteProfile: bindActionCreators(deleteProfile, dispatch),
-     clickOnEntityTag: bindActionCreators(clickOnEntityTag, dispatch),
-   };
- }
+function mapDispatchToProps(dispatch) {
+  return {
+    loadProfiles: bindActionCreators(loadProfiles, dispatch),
+    createProfile: bindActionCreators(createProfile, dispatch),
+    editProfile: bindActionCreators(editProfile, dispatch),
+    cancelEditProfile: bindActionCreators(cancelEditProfile, dispatch),
+    saveProfile: bindActionCreators(saveProfile, dispatch),
+    deleteProfile: bindActionCreators(deleteProfile, dispatch),
+    clickOnProfileTag: bindActionCreators(clickOnProfileTag, dispatch),
+    clickOnEntityTag: bindActionCreators(clickOnEntityTag, dispatch),
+    clickOnEntityDelete: bindActionCreators(clickOnEntityDelete, dispatch),
+    clickOnEntity: bindActionCreators(clickOnEntity, dispatch),
+  };
+}
 
- export default connect(mapStateToProps, mapDispatchToProps)(requiresAuth(ProfilesPage));
+export default connect(mapStateToProps, mapDispatchToProps)(requiresAuth(ProfilesPage));
