b582d096473c63c24d28354d6979e293372b4293
Hung-Han Chen <chenhungh@gmail.com>
2016-11-27 21:57:28 +0200

    new condition if dime return 500 error

--- app/utils/request.js
+++ app/utils/request.js
@@ -24,6 +24,11 @@ function checkStatus(response) {
   if (response.status >= 200 && response.status < 300) {
     return response;
   }
+  if (response.status === 500) {
+    const error = new Error(response.statusText);
+    error.response = {};
+    throw error;
+  }
   const error = new Error(response.statusText);
   error.response = response;
   throw error;
