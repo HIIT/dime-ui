2263e1c942a00b1fd7f2afce46ad601aa51e2f00
Hung-Han Chen <chenhungh@gmail.com>
2017-01-09 11:46:24 +0200

    Revert "revert changes"
    This reverts commit 83c7ab7908f0fdf31b2ead16cb4c4eee522cf80d.

---
+++ app/components/VersionNumber/index.js
@@ -0,0 +1,24 @@
+/**
+*
+* VersionNumber
+*
+*/
+
+import React from 'react';
+
+import styles from './styles.css';
+
+class VersionNumber extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  static propTypes = {
+    value: React.PropTypes.number,
+  };
+  render() {
+    return (
+      <div className={styles.versionNumber}>
+        <span>{this.props.value}</span>
+      </div>
+    );
+  }
+}
+
+export default VersionNumber;
