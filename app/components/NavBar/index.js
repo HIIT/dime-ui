a1fd121c0c237a7bd0e680823bb708dd847b54f8
Mats Sjöberg <mats.sjoberg@helsinki.fi>
2017-03-16 14:08:50 +0200

    Moved version number so it is visible.

--- app/components/NavBar/index.js
+++ app/components/NavBar/index.js
@@ -54,8 +54,10 @@ class NavBar extends React.Component { // eslint-disable-line react/prefer-state
                 DiMe
               </span>
             </div>
-          </Link>
-          <VersionNumber value={this.props.versionNumber} getVersionNumber={this.props.getVersionNumber} />
+            </Link>
+            <div className={styles.navBarVersionNumberWrapper}>
+              <VersionNumber value={this.props.versionNumber} getVersionNumber={this.props.getVersionNumber} />
+            </div>
           { this.props.pathName === '/signin' ? null :
             <div className={styles.navBarMenuWrapper}>
               <DropDownMenu value={this.props.pathName} onChange={this.handleChange}>
